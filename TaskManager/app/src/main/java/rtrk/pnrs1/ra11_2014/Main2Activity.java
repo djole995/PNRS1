a9d22471e7d81a862cdb8b77752bb0a22ca6b905
Milan Djokic <milandjokic1995@gmail.com>
2017-03-25 18:04:15 +0100

    proba

--- TaskManager/app/src/main/java/rtrk/pnrs1/ra11_2014/Main2Activity.java
+++ TaskManager/app/src/main/java/rtrk/pnrs1/ra11_2014/Main2Activity.java
@@ -5,61 +5,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
 
     int attrSetCnt = 0;
     int attrNumber = 4;
+    final Button addTaskBtn = (Button) findViewById(R.id.btnAdd);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-
-
-        final Button setTaskNameBtn = (Button) findViewById(R.id.btnConfirm1);
-        final Button setTaskTimeBtn = (Button) findViewById(R.id.btnConfirm2);
-        final Button setTaskDescBtn = (Button) findViewById(R.id.btnConfirm3);
         final Button btnPriority1 = (Button) findViewById(R.id.btnRed);
         final Button btnPriority2 = (Button) findViewById(R.id.btnYellow);
         final Button btnPriority3 = (Button) findViewById(R.id.btnGreen);
-        final Button addTaskBtn = (Button) findViewById(R.id.btnAdd);
+
         final Button cancelBtn = (Button) findViewById(R.id.btnCancel);
 
         final Intent in = new Intent(Main2Activity.this, MainActivity.class);
-
-        setTaskNameBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(++attrSetCnt == attrNumber)
-                {
-                    addTaskBtn.setEnabled(true);
-                }
-                setTaskNameBtn.setEnabled(false);
-            }
-        });
-
-        setTaskTimeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(++attrSetCnt == attrNumber)
-                {
-                    addTaskBtn.setEnabled(true);
-                }
-                setTaskTimeBtn.setEnabled(false);
-            }
-        });
-
-        setTaskDescBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(++attrSetCnt == attrNumber)
-                {
-                    addTaskBtn.setEnabled(true);
-                }
-                setTaskDescBtn.setEnabled(false);
-            }
-        });
 
         btnPriority1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,5 +76,21 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void onClickEditText(EditText editText) {
+       // editText.setEnabled(true);
+        editText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+    }
+
+    public void onClickConfirmButton(Button btnConfirm) {
+        EditText attrText = (EditText) findViewById(R.id.TaskName);
+        if(++attrSetCnt == attrNumber) {
+            addTaskBtn.setEnabled(true);
+        }
+
+        btnConfirm.setEnabled(false);
+
+        attrText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
     }
 }
