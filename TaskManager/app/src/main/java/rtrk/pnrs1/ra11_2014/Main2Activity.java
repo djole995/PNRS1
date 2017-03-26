a246ec126fab598722541afb02cc98f6f7403de5
Milan Djokic <milandjokic1995@gmail.com>
2017-03-26 15:09:50 +0200

    Izdvojene onClick metode za  svaki confirm button

--- TaskManager/app/src/main/java/rtrk/pnrs1/ra11_2014/Main2Activity.java
+++ TaskManager/app/src/main/java/rtrk/pnrs1/ra11_2014/Main2Activity.java
@@ -6,6 +6,8 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,11 +17,18 @@ public class Main2Activity extends AppCompatActivity {
     protected int attrSetCnt;
     protected int attrNumber;
     protected boolean prioritySelected;
-    protected Button addTaskBtn;
+    protected Button btnAddTask;
     protected Button btnPriority1;
     protected Button btnPriority2;
     protected Button btnPriority3;
-    protected Button cancelBtn;
+    protected Button btnCancel;
+    protected Button btnConfirmTaskName;
+    protected Button btnConfirmTaskTime;
+    protected Button btnConfirmTaskDesc;
+    protected EditText txtTaskName;
+
+    protected Layout taskTime;
+    protected EditText txtTaskDescription;
     protected Intent in;
 
     @Override
@@ -27,14 +36,18 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        addTaskBtn = (Button) findViewById(R.id.btnAdd);
+        btnAddTask = (Button) findViewById(R.id.btnAdd);
         prioritySelected = false;
         attrSetCnt = 0;
         attrNumber = 4;
+        btnConfirmTaskName = (Button) findViewById((R.id.btnConfirm1));
+        btnConfirmTaskTime = (Button) findViewById((R.id.btnConfirm2));
+        btnConfirmTaskDesc = (Button) findViewById((R.id.btnConfirm3));
         btnPriority1 = (Button) findViewById(R.id.btnRed);
         btnPriority2 = (Button) findViewById(R.id.btnYellow);
         btnPriority3 = (Button) findViewById(R.id.btnGreen);
-        cancelBtn = (Button) findViewById(R.id.btnCancel);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        txtTaskName = (EditText) findViewById(R.id.day);
         in = new Intent(Main2Activity.this, MainActivity.class);
 
         btnPriority1.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +56,7 @@ public class Main2Activity extends AppCompatActivity {
 
                 if(!prioritySelected) {
                     if(++attrSetCnt == attrNumber) {
-                        addTaskBtn.setEnabled(true);
+                        btnAddTask.setEnabled(true);
                     }
                     btnPriority2.setEnabled(false);
                     btnPriority2.setAlpha(0.2f);
@@ -53,7 +66,7 @@ public class Main2Activity extends AppCompatActivity {
                 }
                 else {
                     --attrSetCnt;
-                    addTaskBtn.setEnabled(false);
+                    btnAddTask.setEnabled(false);
                     btnPriority2.setEnabled(true);
                     btnPriority2.setAlpha(1.0f);
                     btnPriority3.setEnabled(true);
@@ -68,7 +81,7 @@ public class Main2Activity extends AppCompatActivity {
             public void onClick(View v) {
                 if(!prioritySelected) {
                     if(++attrSetCnt == attrNumber) {
-                        addTaskBtn.setEnabled(true);
+                        btnAddTask.setEnabled(true);
                     }
                     btnPriority1.setEnabled(false);
                     btnPriority1.setAlpha(0.2f);
@@ -78,7 +91,7 @@ public class Main2Activity extends AppCompatActivity {
                 }
                 else {
                     --attrSetCnt;
-                    addTaskBtn.setEnabled(false);
+                    btnAddTask.setEnabled(false);
                     btnPriority1.setEnabled(true);
                     btnPriority1.setAlpha(1.0f);
                     btnPriority3.setEnabled(true);
@@ -93,7 +106,7 @@ public class Main2Activity extends AppCompatActivity {
             public void onClick(View v) {
                 if(!prioritySelected) {
                     if(++attrSetCnt == attrNumber) {
-                        addTaskBtn.setEnabled(true);
+                        btnAddTask.setEnabled(true);
                     }
                     btnPriority1.setEnabled(false);
                     btnPriority1.setAlpha(0.2f);
@@ -103,7 +116,7 @@ public class Main2Activity extends AppCompatActivity {
                 }
                 else {
                     --attrSetCnt;
-                    addTaskBtn.setEnabled(false);
+                    btnAddTask.setEnabled(false);
                     btnPriority1.setEnabled(true);
                     btnPriority1.setAlpha(1.0f);
                     btnPriority2.setEnabled(true);
@@ -113,41 +126,73 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
-        addTaskBtn.setOnClickListener(new View.OnClickListener() {
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(in);
             }
         });
 
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
+        btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(in);
             }
         });
 
-    }
-
-    public  void onClickEditText(View view) {
-        EditText editText = (EditText) findViewById(view.getId());
-
-    }
-
-    public void onClickConfirmButton(View view) {
-        Button btnConfirm = (Button) findViewById(view.getId());
-
-        if(btnConfirm.getText().equals(getString(R.string.confirm))) {
-            if(++attrSetCnt == attrNumber) {
-                addTaskBtn.setEnabled(true);
+        btnConfirmTaskName.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(btnConfirmTaskName.getText().equals(getString(R.string.confirm))) {
+                    if(++attrSetCnt == attrNumber) {
+                        btnAddTask.setEnabled(true);
+                    }
+                    btnConfirmTaskName.setText(R.string.change);
+                }
+                else {
+                    attrSetCnt--;
+                    btnAddTask.setEnabled(false);
+                    btnConfirmTaskName.setText(R.string.confirm);
+                }
             }
-            btnConfirm.setText(R.string.change);
-        }
-        else {
-            attrSetCnt--;
-            addTaskBtn.setEnabled(false);
-            btnConfirm.setText(R.string.confirm);
-        }
+        });
+
+        btnConfirmTaskTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(btnConfirmTaskTime.getText().equals(getString(R.string.confirm))) {
+                    if(++attrSetCnt == attrNumber) {
+                        btnAddTask.setEnabled(true);
+                    }
+                    btnConfirmTaskTime.setText(R.string.change);
+                }
+                else {
+                    attrSetCnt--;
+                    btnAddTask.setEnabled(false);
+                    btnConfirmTaskTime.setText(R.string.confirm);
+                }
+
+            }
+        });
+
+        btnConfirmTaskDesc.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(btnConfirmTaskDesc.getText().equals(getString(R.string.confirm))) {
+                    if(++attrSetCnt == attrNumber) {
+                        btnAddTask.setEnabled(true);
+                    }
+                    btnConfirmTaskDesc.setText(R.string.change);
+                }
+                else {
+                    attrSetCnt--;
+                    btnAddTask.setEnabled(false);
+                    btnConfirmTaskDesc.setText(R.string.confirm);
+                }
+
+            }
+        });
+
     }
 
 }
