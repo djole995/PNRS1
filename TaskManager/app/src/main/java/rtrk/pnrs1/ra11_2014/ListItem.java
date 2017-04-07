d759be642df811775b7e312c5ef9c5123e049a63
Milan Djokic <milandjokic1995@gmail.com>
2017-04-08 00:07:44 +0200

    Zadatak 2

---
+++ TaskManager/app/src/main/java/rtrk/pnrs1/ra11_2014/ListItem.java
@@ -0,0 +1,20 @@
+package rtrk.pnrs1.ra11_2014;
+
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.Date;
+
+/**
+ * Created by Milan on 7.4.2017.
+ */
+
+public class ListItem {
+
+    protected TextView taskPriority;
+    protected TextView taskName;
+    protected String taskDate;
+    protected RadioButton taskReminder;
+    protected CheckBox taskFinished;
+}
--- TaskManager/app/src/main/java/rtrk/pnrs1/ra11_2014/MainActivity.java
+++ TaskManager/app/src/main/java/rtrk/pnrs1/ra11_2014/MainActivity.java
@@ -1,19 +1,22 @@
 package rtrk.pnrs1.ra11_2014;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_item);
 
-        Button btnNewTask = (Button) findViewById(R.id.btnNewTask);
+/*        Button btnNewTask = (Button) findViewById(R.id.btnNewTask);
         Button btnStat = (Button) findViewById(R.id.btnStat);
         final Intent inNewTask = new Intent(MainActivity.this, Main2Activity.class);
         final Intent inStat = new Intent(MainActivity.this, Main3Activity.class);
@@ -30,6 +33,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(inStat);
             }
-        });
+        });*/
     }
 }
